47001f4d676f907333182f0480d1450388feaeae
Francois Salmon <francois.salmon@semifir.com>
2025-07-09 13:58:28 +0200

    test maven

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,7 +2,8 @@ package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -26,6 +27,6 @@ public class AppTest
     @Test
     public void testApp()
     {
-        assertTrue(false); // forcer l’échec
+        assertTrue(true); // forcer l’échec
     }
 }
